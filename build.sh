1545bf7cdb1ccac5440d64dd540a72e34700fffa
Zach Garwood <zachgarwood@gmail.com>
2015-08-20 00:24:40 -0500

    Removed unnecessary build files

--- build.sh
+++
@@ -1,4 +0,0 @@
-mkdir panopta_rest_api
-cp test_api_client.php api_client.php LICENSE  README panopta_rest_api
-tar cvfz panopta_rest_api-php-`cat VERSION`.tgz panopta_rest_api
-rm -rf panopta_rest_api
